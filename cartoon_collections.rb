967d461f9a2c4612f0e08d7374765488272342ae
Berkley Brown <berkleydeane@gmail.com>
2020-06-01 16:06:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,10 +13,8 @@ def summon_captain_planet(array)
  while i < array.length
   new_array << array[i].capitalize + "!"
    i += 1
- 
- 
  end
-  
+  new_array
 end
 
 def long_planeteer_calls# code an argument here
