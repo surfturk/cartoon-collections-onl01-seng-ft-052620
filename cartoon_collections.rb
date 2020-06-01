923b87961935223f4286c1e21a922e67388bced9
Berkley Brown <berkleydeane@gmail.com>
2020-06-01 16:10:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,10 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  
+  if array.length > 4
+    puts true
+  else
+    false
 end
 
 def find_the_cheese# code an argument here
